1cc04b4ff446363bd958ae1b8643be79aa26b3e5
Jeekykidspo <bentahirfares@gmail.com>
2024-09-20 16:24:58 +0200

    Update ecoledirect.py

--- ecoledirect.py
+++ ecoledirect.py
@@ -23,8 +23,8 @@ menu = f"""
                 │[8]Proxy_Connect(bypassauth) │[25]Comming soon            │[42]CC Checker                       │[58]Fake BTC Wallet Miner       │
                 │[9]Osint_Compte_Ecoledirect  │[26]J4J_Discord_Bot         │[43]Nitro Gen                        │[59]IP Grabber PY               │
                 │[10]Phishing_Ecoledirect_tool│[27]iplookup                │[44]Nitro gen 2                      │[60]Insta Checker               │
-                │[11]Database_Ecoledirect_Acc │[28]Hazard_Nuker            │[45]Nitro Gen 3                      │[61]Fake Discord IP Puller      │
-                │[12]Comming soon             │[29]               │[46]Random Multi Tool                │[62]TikTok Checker              │
+                │[11]Database_Ecoledirect_Acc │[28]Comming soon │[45]Nitro Gen 3                      │[61]Fake Discord IP Puller      │
+                │[12]Comming soon             │[29]Comming soon            │[46]Random Multi Tool                │[62]TikTok Checker              │
                 │[13]Comming soon             │[30]Comming soon            │[47]Discord Rat                      │[63]TikTok Autoclaimer          │
                 │[14]Comming soon             │[31]Comming soon            │[48]csgo Cheats+bypass               │[64]TikTok 4L/4C Checker        │
                 │[15]Comming soon             │[32]Comming soon            │[49]Free VPN                         │[65]Fivem Executor              │
