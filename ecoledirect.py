b59075f153b14597c402774807a5623fb2a528e2
Jeekykidspo <bentahirfares@gmail.com>
2024-09-20 16:22:09 +0200

    Update ecoledirect.py

--- ecoledirect.py
+++ ecoledirect.py
@@ -2,7 +2,7 @@ import os, colorama, webbrowser, time; from colorama import *
 
 os.system('color D')
 
-os.system('cls & title Made by danger ')
+os.system('cls & title Made by Danger. Remastered by skider with own option ')
 
 menu = f"""
                                               ██████╗  █████╗ ███╗   ██╗ ██████╗ ███████╗██████╗ 
@@ -14,25 +14,22 @@ menu = f"""
                                               
                                                                                 
                 │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━│
-                │[1]discord_token.genV3       │[18]Velocity-Grabber(PIAD)  │[35]TWITCH_Follower_Bot              │[52]DISCORDEN                   │
-                │[2]Discord_Nitro_Generator   │[19]stresser                │[36]Twitch_Token_Gen                 │[53]ID TO TOKEN                 │
-                │[4]storecard_gen             │[21]Tiktok_View_Bot         │[38]roblox_pin_cracker               │[54]Discord Promo Gen           │
-                │[5]amazon_giftcared_gen      │[22]Giftcard_Gen            │[39]Account Gen                      │[55]ValidCardCredit Card Gen    │
-                │[6]boost_bot                 │[23]RexNuker                │[40]Twitch_Token_Gen/Checker         │[56]Zeogen                      │ 
-                │[7]Fake-Verification-Bot-main│[24]LithiumNuker            │[41]CC Gen                           │[57]Fake ETH Wallet Miner       │
-                │[8]Pinger                    │[25]KyaniteNuker            │[42]CC Checker                       │[58]Fake BTC Wallet Miner       │
-                │[9]Token_Nuke                │[26]J4J_Discord_Bot         │[43]Nitro Gen                        │[59]IP Grabber PY               │
-                │[10]putty                    │[27]iplookup                │[44]Nitro gen 2                      │[60]Insta Checker               │
-                │[11]NaamsPanel               │[28]Hazard_Nuker            │[45]Nitro Gen 3                      │[61]Fake Discord IP Puller      │
-                │[12]dox_tool                 │[29]GangNuker               │[46]Random Multi Tool                │[62]TikTok Checker              │
-                │[13]Dox_tool_v2              │[30]empyrean_grabber        │[47]Discord Rat                      │[63]TikTok Autoclaimer          │
-                │[14]Discord.Raidkit.v1       │[31]Discord_Mass_Dm         │[48]csgo Cheats+bypass               │[64]TikTok 4L/4C Checker        │
-                │[15]Discord-Account-Ruiner   │[32]Discord token_gen       │[49]Free VPN                         │[65]Fivem Executor              │
-                │[16]DiscordTokenChecker      │[33]Ascii gen               │[50]Free Windows                     │[66]Despasudo (FIXXEN)          │
-                │[17]Discord_Image_Grabber    │[34]discord-gen-bot         │[51]Discord boost Service PAC        │[67]C2 Botnets IDk Bu ye        │
-              import os, colorama, webbrowser, time; from colorama import *
-
-
+                │[1]Gen_Chat_Ecoledirect      │[18]Comming soon            │[35]TWITCH_Follower_Bot              │[52]DISCORDEN                   │
+                │[2]Subdomain_Ecoledirect     │[19]Comming soon            │[36]Twitch_Token_Gen                 │[53]ID TO TOKEN                 │
+                │[4]Some_Site_Ecoledirect     │[21]Comming soon            │[38]roblox_pin_cracker               │[54]Discord Promo Gen           │
+                │[5]Ip_EcoleDirect            │[22]Comming soon            │[39]Account Gen                      │[55]ValidCardCredit Card Gen    │
+                │[6]Ddos_Ecoledirect          │[23]Comming soon            │[40]Twitch_Token_Gen/Checker         │[56]Zeogen                      │ 
+                │[7]Brute_Force_tool          │[24]Comming soon            │[41]CC Gen                           │[57]Fake ETH Wallet Miner       │
+                │[8]Proxy_Connect(bypassauth) │[25]Comming soon            │[42]CC Checker                       │[58]Fake BTC Wallet Miner       │
+                │[9]Osint_Compte_Ecoledirect  │[26]J4J_Discord_Bot         │[43]Nitro Gen                        │[59]IP Grabber PY               │
+                │[10]Phishing_Ecoledirect_tool│[27]iplookup                │[44]Nitro gen 2                      │[60]Insta Checker               │
+                │[11]Database_Ecoledirect_Acc │[28]Hazard_Nuker            │[45]Nitro Gen 3                      │[61]Fake Discord IP Puller      │
+                │[12]Comming soon             │[29]               │[46]Random Multi Tool                │[62]TikTok Checker              │
+                │[13]Comming soon             │[30]Comming soon            │[47]Discord Rat                      │[63]TikTok Autoclaimer          │
+                │[14]Comming soon             │[31]Comming soon            │[48]csgo Cheats+bypass               │[64]TikTok 4L/4C Checker        │
+                │[15]Comming soon             │[32]Comming soon            │[49]Free VPN                         │[65]Fivem Executor              │
+                │[16]Comming soon             │[33]Comming soon            │[50]Free Windows                     │[66]Despasudo (FIXXEN)          │
+                │[17]Comming soon             │[34]Comming soon            │[51]Discord boost Service PAC        │[67]C2 Botnets IDk Bu ye        │          
 choice = int(input('    Choose: '))
 
 
